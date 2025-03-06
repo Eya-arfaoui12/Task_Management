2ddd7af8169ea79856a9a3b563574b06666544af
sarahmh1 <Sarah.mhadhbi17@gmail.com>
2025-03-06 13:02:44 +0100

    add new task

--- frontend/src/app/task.service.ts
+++ frontend/src/app/task.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { WebRequestService } from './web-request.service';
 import { Observable } from 'rxjs';
+import { List } from './models/list.model';
+import { Task } from './models/task.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +11,21 @@ export class TaskService {
 
   constructor(private webReqService: WebRequestService) { }
 
-  createList(title: string) {
-    //we want to send a web request to create a list
-    return this.webReqService.post('lists', {title});
-  }
-
   getLists() {
     return this.webReqService.get('lists');
   }
 
+  createList(title: string): Observable<List> {
+    //we want to send a web request to create a list
+    return this.webReqService.post<List>('lists', {title});
+  }
+
   getTasks(listId: string): Observable<any[]>{
     return this.webReqService.get(`lists/${listId}/tasks`);
+  }
+ 
+  createTask(title: string, listId: string): Observable<Task> {
+    //we want to send a web request to create a task
+    return this.webReqService.post<Task>(`lists/${listId}/tasks`, {title});
   }
 }
